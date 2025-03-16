e968b3e1785fcb5f3de6476c322fccf1c832e1ba
AakashTiwari-nitp <aakasht.ug23.cs@nitp.ac.in>
2025-03-16 10:09:39 +0530

    improved transition of navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,13 +2,14 @@
 import Link from "next/link";
 import React from "react";
 import Image from "next/image";
-import { useState, useEffect,useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import { navItems } from "./landing/navItems";
 import { BoldIcon, ChevronDown, Menu, X } from "lucide-react";
 import { AiOutlineClose } from "react-icons/ai";
 import { useAutoAnimate } from "@formkit/auto-animate/react";
 import { IoIosArrowDown, IoIosArrowDropright } from "react-icons/io";
-import "@/components/styles/navbar.css"
+import "@/components/styles/navbar.css";
+
 export const Topbar = () => {
   const rightSide = [
     { href: "/notices/jobsNitp", text: "Jobs@NITP" },
@@ -49,7 +50,7 @@ export const Topbar = () => {
           <Link
             key={index}
             href={item.href}
-            className="hover:scale-105 active:scale-95 transition-transform duration-200 ease-in-out ring-1 ring-gray-300/50 shadow-md md:shadow-lg py-2 px-1 md:px-4 md:py-2 text-xs md:text-sm rounded-xl bg-gradient-to-b from-gray-100/20 to-gray-800/30 backdrop-blur-md"
+            className="hover:scale-105 active:scale-95 transition-transform duration-200 ease-in-out ring-1 ring-gray-300/50 shadow-md md:shadow-lg py-1 px-1 md:px-4 md:py-1 text-xs md:text-sm rounded-xl bg-gradient-to-b from-gray-100/20 to-gray-800/30 backdrop-blur-md"
           >
             {item.text}
           </Link>
@@ -83,7 +84,7 @@ export const Middle = () => {
 
   useEffect(() => {
     const handleScroll = () => {
-      setIsScrolled(window.scrollY > 50);
+      setIsScrolled(window.scrollY > 25);
     };
 
     window.addEventListener("scroll", handleScroll);
@@ -93,10 +94,11 @@ export const Middle = () => {
   return (
     <header
       className={`sticky top-0 w-full transition-all duration-300 ${
-        isScrolled ? "backdrop-blur-lg bg-white/60 " : "bg-white"
+        isScrolled ? "backdrop-blur-lg bg-white/60" : "bg-white"
       }`}
     >
-      <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center px-6 py-1 md:py-1">
+      <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center px-6">
+        {/* mobile */}
         <div className="flex md:hidden items-center justify-between w-full">
           <Link href="/">
             <Image
@@ -104,8 +106,8 @@ export const Middle = () => {
               alt="NIT PATNA"
               width={50}
               height={50}
-              className={`transition-transform duration-300 ${
-                isScrolled ? "scale-90" : "scale-100"
+              className={`transition-transform duration-500 ${
+                isScrolled ? "scale-80" : "scale-100"
               }`}
             />
           </Link>
@@ -118,9 +120,14 @@ export const Middle = () => {
             </span>
           </div>
         </div>
-        <div className="hidden md:flex items-center justify-between w-full">
+
+        {/* laptop */}
+        <div
+          className={`hidden md:flex items-center justify-between w-full transition-all duration-500 ease-in-out
+    ${isScrolled ? "h-16" : "h-24"}`}
+        >
           {/* Left Content */}
-          <div className="text-gray-900">
+          <div className="text-gray-900 transition-all duration-500 ease-in-out">
             <h1 className="text-xl font-semibold tracking-wide">
               राष्ट्रीय प्रौद्योगिकी संस्थान पटना
             </h1>
@@ -129,20 +136,20 @@ export const Middle = () => {
             </p>
           </div>
 
-          <Link href="/" className="">
-            <Image
-              src={"/nitp-logo.svg"}
-              alt="NIT PATNA"
-              width={90}
-              height={90}
-              className={`transition-transform duration-300 hover:scale-110 ${
-                isScrolled
-                  ? "scale-75 -translate-y-1 "
-                  : "scale-105 translate-y-0 border-rose-700"
-              }`}
-            />
+          <Link href="/">
+            <div
+              className={`overflow-hidden transition-all duration-500 ease-in-out
+        ${isScrolled ? "h-16 w-16" : "h-24 w-24"}`}
+            >
+              <img
+                src="/nitp-logo.svg"
+                alt="NIT PATNA"
+                className="h-full w-full"
+              />
+            </div>
           </Link>
-          <div className="text-center text-gray-800">
+
+          <div className="text-center text-gray-800 transition-all duration-500 ease-in-out">
             <p className="text-md font-medium">
               An Institute of National Importance
             </p>
@@ -155,34 +162,53 @@ export const Middle = () => {
     </header>
   );
 };
-export const BottomNav = () => { 
+
+export const BottomNav = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 30);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const [isSideMenuOpen, setSideMenuOpen] = useState(false);
+
   return (
     <>
-    <nav className="bg-red-700 text-white backdrop-blur-xl  xl:px-6 py-3 md:flex hidden md:gap-0 xl:gap-6 items-center justify-evenly w-full shadow-lg backdrop-filter transition duration-300">
-      {navItems.map((item, index) =>
-        item.dropdown ? (
-          <Dropdown
-            key={index}
-            title={item.name}
-            items={item.dropdown}
-            icon={item.icon}
-          />
-        ) : (
-          <Link
-            key={index}
-            href={item.url}
-            className="flex items-center gap-2 hover:opacity-80"
-          >
-            {item.icon}
-            {item.name}
-          </Link>
-        )
-      )}
-    </nav>
+      <nav
+        className={`bg-red-700 text-white backdrop-blur-xl xl:px-6 ${
+          isScrolled
+            ? "py-1 transition-transform duration-500"
+            : "py-2 transition-transform duration-500"
+        } md:flex hidden md:gap-0 xl:gap-6 items-center justify-evenly w-full shadow-lg backdrop-filter transition duration-300`}
+      >
+        {navItems.map((item, index) =>
+          item.dropdown ? (
+            <Dropdown
+              key={index}
+              title={item.name}
+              items={item.dropdown}
+              icon={item.icon}
+            />
+          ) : (
+            <Link
+              key={index}
+              href={item.url}
+              className="flex items-center gap-2 hover:opacity-80"
+            >
+              {item.icon}
+              {item.name}
+            </Link>
+          )
+        )}
+      </nav>
 
-    <div className="md:hidden flex items-center justify-between bg-red-700 text-white px-4 py-3">
+      <div className="md:hidden flex items-center justify-between bg-red-700 text-white px-4 py-3">
         <button onClick={() => setSideMenuOpen(true)}>
           <Menu size={28} />
         </button>
@@ -202,14 +228,20 @@ export const BottomNav = () => {
         <div className="flex flex-col gap-2 px-6">
           {navItems.map((item, index) =>
             item.dropdown ? (
-              <Dropdown key={index} title={item.name} items={item.dropdown} icon={item.icon} mobile />
+              <Dropdown
+                key={index}
+                title={item.name}
+                items={item.dropdown}
+                icon={item.icon}
+                mobile
+              />
             ) : (
               <Link
                 key={index}
                 href={item.url}
                 className="flex items-center gap-2 py-2 hover:bg-red-900 rounded-md px-3"
                 // onClick={() => setIsMobileMenuOpen(false)}
-                onClick={()=>setSideMenuOpen(false)}
+                onClick={() => setSideMenuOpen(false)}
               >
                 {item.icon}
                 {item.name}
@@ -219,15 +251,22 @@ export const BottomNav = () => {
         </div>
       </div>
       {isMobileMenuOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 z-40" onClick={() => setIsMobileMenuOpen(false)}></div>
+        <div
+          className="fixed inset-0 bg-black bg-opacity-50 z-40"
+          onClick={() => setIsMobileMenuOpen(false)}
+        ></div>
       )}
       {/* <BoldIcon
             onClick={() => setSideMenuOpen(true)} 
             className="cursor-pointer text-3xl md:hidden text-black ml-auto" 
           /> */}
-    {isSideMenuOpen && <MobileNav closeSideMenu={() => setSideMenuOpen(false)} navItems={navItems} />}
+      {isSideMenuOpen && (
+        <MobileNav
+          closeSideMenu={() => setSideMenuOpen(false)}
+          navItems={navItems}
+        />
+      )}
     </>
-
   );
 };
 
@@ -235,10 +274,17 @@ function MobileNav({ closeSideMenu }) {
   return (
     <div className="mobile-nav text-black">
       <div className="mobile-nav-content text-black">
-        <AiOutlineClose onClick={closeSideMenu} className="mobile-nav-close text-4xl text-black z-[1000]" />
+        <AiOutlineClose
+          onClick={closeSideMenu}
+          className="mobile-nav-close text-4xl text-black z-[1000]"
+        />
         <div className="flex flex-col text-base gap-2 transition-all">
           {navItems.map((item, index) => (
-            <SingleNavItem key={index} item={item} closeSideMenu={closeSideMenu} />
+            <SingleNavItem
+              key={index}
+              item={item}
+              closeSideMenu={closeSideMenu}
+            />
           ))}
         </div>
       </div>
@@ -246,34 +292,37 @@ function MobileNav({ closeSideMenu }) {
   );
 }
 
-
-
-
-
-function SingleNavItem({ item ,closeSideMenu}) {
+function SingleNavItem({ item, closeSideMenu }) {
   const [animationParent] = useAutoAnimate();
   const [isItemOpen, setItemOpen] = useState(false);
- 
+
   return (
     <div ref={animationParent} className="relative px-1 py-3 transition-all">
-      <p onClick={() => setItemOpen(!isItemOpen)} className="flex cursor-pointer items-center gap-2 text-neutral-900 group-hover:text-black">
-      {item.icon}
-      <Link href={ "#"} onClick={closeSideMenu} >
-                {item.name}
-              </Link>
-        {item.dropdown && <IoIosArrowDown className={`text-xs transition-all ${isItemOpen && "rotate-180"}`} />}
+      <p
+        onClick={() => setItemOpen(!isItemOpen)}
+        className="flex cursor-pointer items-center gap-2 text-neutral-900 group-hover:text-black"
+      >
+        {item.icon}
+        <Link href={"#"} onClick={closeSideMenu}>
+          {item.name}
+        </Link>
+        {item.dropdown && (
+          <IoIosArrowDown
+            className={`text-xs transition-all ${isItemOpen && "rotate-180"}`}
+          />
+        )}
       </p>
       {isItemOpen && item.dropdown && (
         <div className="w-auto flex-col gap-1  bg-neutral-50 py-3 transition-all flex">
           {item.dropdown.map((child, index) => (
-            <SubSidemenu key={index} item={child}/>
+            <SubSidemenu key={index} item={child} />
           ))}
         </div>
       )}
     </div>
   );
 }
-function SubSidemenu({ item,closeSideMenu }) {
+function SubSidemenu({ item, closeSideMenu }) {
   const [isSubItemOpen, setSubItemOpen] = useState(false);
   const [isItemOpen, setItemOpen] = useState(true);
   const handleSubToggle = () => {
@@ -282,19 +331,31 @@ function SubSidemenu({ item,closeSideMenu }) {
 
   return (
     <div className="relative px-1 py-1 transition-all ">
-      <p onClick={handleSubToggle} className="flex cursor-pointer items-center gap-1 text-neutral-700 group-hover:text-black">
-      {item.icon}
+      <p
+        onClick={handleSubToggle}
+        className="flex cursor-pointer items-center gap-1 text-neutral-700 group-hover:text-black"
+      >
+        {item.icon}
         <Link href={item.url ?? "#"} onClick={closeSideMenu}>
-        <span>{item.name}</span>
-              </Link>
-        {item.dropdown && <IoIosArrowDown className={`text-xs transition-all ${isSubItemOpen && "rotate-180"} `} />}
+          <span>{item.name}</span>
+        </Link>
+        {item.dropdown && (
+          <IoIosArrowDown
+            className={`text-xs transition-all ${
+              isSubItemOpen && "rotate-180"
+            } `}
+          />
+        )}
       </p>
       {isSubItemOpen && item.dropdown && (
         <div className="w-auto flex-col gap-1 bg-white py-1 transition-all text-sm">
           {item.dropdown.map((subChild, index) => (
             <p key={index} className="flex pl-4">
               {item.icon}
-              <Link href={subChild.url ?? "#"} className="flex cursor-pointer items-center py-1 text-neutral-700 hover:text-black pl-1">
+              <Link
+                href={subChild.url ?? "#"}
+                className="flex cursor-pointer items-center py-1 text-neutral-700 hover:text-black pl-1"
+              >
                 <span>{subChild.name}</span>
               </Link>
             </p>
@@ -304,14 +365,15 @@ function SubSidemenu({ item,closeSideMenu }) {
     </div>
   );
 }
-const Dropdown = ({ title, items, icon,mobile=false }) => {
-  
+
+const Dropdown = ({ title, items, icon, mobile = false }) => {
   const [isHovered, setIsHovered] = useState(false);
   const [hoveredItem, setHoveredItem] = useState(null);
   const dropdownRef = useRef(null);
   const subDropdownRef = useRef(null);
   const [dropdownPosition, setDropdownPosition] = useState("top-full left-0");
-  const [subDropdownPosition, setSubDropdownPosition] = useState("left-full top-0");
+  const [subDropdownPosition, setSubDropdownPosition] =
+    useState("left-full top-0");
 
   useEffect(() => {
     if (isHovered && dropdownRef.current) {
@@ -323,7 +385,7 @@ const Dropdown = ({ title, items, icon,mobile=false }) => {
       if (rect.bottom > screenHeight) {
         newPosition = "bottom-full left-0";
       }
-      if (rect.right+500 > screenWidth) {
+      if (rect.right + 500 > screenWidth) {
         newPosition = "top-full right-0";
       }
 
@@ -357,7 +419,10 @@ const Dropdown = ({ title, items, icon,mobile=false }) => {
     >
       <div className="flex items-center gap-1 cursor-pointer py-2 xl:px-4 rounded-md hover:bg-red-800 transition duration-200">
         <span className="font-medium">{title}</span>
-        <ChevronDown size={18} className="transition-transform duration-300 group-hover:rotate-180" />
+        <ChevronDown
+          size={18}
+          className="transition-transform duration-300 group-hover:rotate-180"
+        />
       </div>
       {isHovered && (
         <div
