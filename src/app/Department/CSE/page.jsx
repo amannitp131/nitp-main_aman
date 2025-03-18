4fbb0c8888eb97ad45fe0d7bc31b8479a79f3700
AakashTiwari-nitp <aakasht.ug23.cs@nitp.ac.in>
2025-03-18 19:53:35 +0530

    updated Dept Page

--- src/app/Department/CSE/page.jsx
+++ src/app/Department/CSE/page.jsx
@@ -1,39 +1,99 @@
 import React from "react";
 import DepartmentCounter from "@/components/department/DeptCounter";
 import DeptNavbar from "@/components/department/DeptNavbar";
+import AcadProgram from "@/components/department/AcadProgram";
 
 const about = `The Department of Computer Science and Engineering offers courses leading to Bachelor of Technology in Computer Science and Engineering. The department has a very good infrastructure and faculty to provide excellent education. The curriculum is updated regularly to keep up with the growing demands and the changing trends of the software industry and research laboratories. The department provides a wide range of courses. The prominent among them includes Data Structures, Design and Analysis of Algorithms, System Programming, Computer Networks, Data Mining and Warehousing, Distributed and Parallel Computing, Mobile and Wireless Computing, Real-time Systems, Cryptography, Genetic Algorithm, Quantum Algorithms and Artificial Intelligence. Apart from these, students are also offered a wide variety of electives.`;
 
 const counts = [
   {
-    name: "Students",
+    name: "Undergraduate Students",
     icon: "/students_icon.png",
     count: 456,
+    color: "#FFCDD2", // Soft Red
+  },
+  {
+    name: "Postgraduate Students",
+    icon: "/students_icon.png",
+    count: 123,
+    color: "#BBDEFB", // Soft Blue
+  },
+  {
+    name: "Ph.D. Students",
+    icon: "/students_icon.png",
+    count: 49,
+    color: "#D1C4E9", // Soft Purple
   },
   {
     name: "Faculty",
     icon: "/faculty_icon.png",
     count: 25,
+    color: "#FFECB3", // Soft Yellow
   },
   {
     name: "Journal",
     icon: "/journal_icon.png",
     count: 25,
+    color: "#C8E6C9", // Soft Green
   },
   {
     name: "Conferences",
     icon: "/conference_icon.png",
     count: 78,
+    color: "#FFCCBC", // Soft Orange
   },
   {
     name: "Projects",
     icon: "/projects_icon.png",
     count: 49,
+    color: "#D7CCC8", // Soft Brown
   },
   {
     name: "Research",
     icon: "/research_icon.png",
     count: 123,
+    color: "#B2DFDB", // Soft Teal
+  },
+  {
+    name: "Patents",
+    icon: "/patents_icon.png",
+    count: 123,
+    color: "#F8BBD0", // Soft Pink
+  },
+];
+
+const AcadProgrammes = [
+  {
+    name: "Under Graduate",
+    degree: "B.Tech",
+    duration: `4`,
+    specialization: ["CSE"],
+    timeTableLink: "",
+    syllabusLink: "",
+  },
+  {
+    name: "Dual Degree",
+    degree: "Integrated B.Tech and M.Tech",
+    duration: `5`,
+    specialization: ["Cyber Security", "Data Science"],
+    timeTableLink: "",
+    syllabusLink: "",
+  },
+  {
+    name: "Post Graduate",
+    degree: "M.Tech",
+    duration: `2`,
+    specialization: ["CSE"],
+    timeTableLink: "",
+    syllabusLink: "",
+  },
+  {
+    name: "Ph.D.",
+    degree: "Post Doctrate",
+    duration: `5+`,
+    specialization: ["CSE"],
+    timeTableLink: "",
+    syllabusLink: "",
   },
 ];
 
@@ -41,19 +101,41 @@ const CSEPage = () => {
   return (
     <div className="bg-white">
       <DeptNavbar dept="CSE" />
-      <h2 className="text-center text-5xl mt-2 text-red-700 uppercase">
+      <h2 className="text-center px-2 text-4xl lg:text-5xl mt-2 text-red-700 uppercase">
         Computer Science And Engineering
       </h2>
 
-      <div className="mt-2 w-[80%] mx-auto py-5 text-justify text-black">
-        {about.split("\n").map((line, index) => (
-          <p key={index} className="mb-2">
-            {line}
-          </p>
-        ))}
+      {/* About the department */}
+      <div className="py-1 mt-2">
+        <div className="flex flex-col lg:flex-row w-full px-5 xs:px-0 md:w-[90%] lg:w-[80%] mx-auto">
+          <div className="w-full">
+            <h2 className="text-center text-4xl text-red-700 mt-2">
+              About The Department
+            </h2>
+            <div className="px-2 flex mx-auto gap-2 flex-1 shrink-0">
+              <div className="w-[100%] mx-auto px-2 py-5 text-justify text-black">
+                {about.split("\n").map((line, index) => (
+                  <p key={index} className="mb-2">
+                    {line}
+                  </p>
+                ))}
+              </div>
+            </div>
+          </div>
+
+          <DepartmentCounter counts={counts} />
+        </div>
       </div>
 
-      <DepartmentCounter counts={counts} />
+      {/* Academic Programmes */}
+      <div className="py-1 mt-2">
+        <h2 className="text-center w-[80%] mx-auto text-4xl text-red-700 mb-2">
+          Academic Programmes
+        </h2>
+        <div className="w-[80%] mx-auto">
+          <AcadProgram acadProgrammes={AcadProgrammes} />
+        </div>
+      </div>
     </div>
   );
 };
